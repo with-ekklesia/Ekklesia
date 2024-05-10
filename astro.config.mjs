9db8027d3cd439b17dc28762f51f5fb94821e7f2
nlopez4 <noahtheark2@gmail.com>
2024-05-10 11:32:37 -0700

    Fly.io deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
-import { defineConfig } from 'astro/config';
-import node from "@astrojs/node";
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
-import cloudflare from "@astrojs/cloudflare";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: cloudflare(),
-  integrations: [tailwind()]
+  adapter: node({
+    mode: "standalone",
+  }),
+  vite: {
+    optimizeDeps: { exclude: ["fsevents"] },
+  },
+  integrations: [tailwind()],
 });
